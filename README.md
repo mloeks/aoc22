afbb5e5fc6d546c7d600e2779450b95c08baba85
Matthias Loeks <matthias-johannes.loeks@xarvio.com>
2022-12-01 11:32:10 +0100

    🎄 Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# AoC 2022
+
+Plain Spring Boot project generated using [Spring Initializr](https://start.spring.io/).
+
+Don't expect sophisticated, beautiful, tested code in here. I'm a family father and 
+just tried to _make it work_, leaving out the _make it nice_, _make it fast_ part ;-)
+
+## Notes on the puzzles
+
---
+++ src/main/java/net/mloeks/aoc22/Aoc22Application.java
@@ -0,0 +1,13 @@
+package net.mloeks.aoc22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aoc22Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aoc22Application.class, args);
+	}
+
+}
